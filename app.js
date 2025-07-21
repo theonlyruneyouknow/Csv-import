2c6c74d4c81111b83966acffe83bbef197bd6ae8
Rune Larsen <theonlyruneyouknow@gmail.com>
2025-07-20 20:45:34 -0700

    3003

--- app.js
+++ app.js
@@ -20,5 +20,5 @@ app.get('/', (req, res) => res.redirect('/purchase-orders'));
 app.get('/upload', (req, res) => res.render('upload'));
 
 app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+    console.log('Server running on http://localhost:3003');
 });
