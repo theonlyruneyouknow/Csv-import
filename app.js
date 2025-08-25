b2e3a5afa0e5e7d677e901c9e4f8a03989374b1e
kalpalloser <72586744+kalpalloser@users.noreply.github.com>
2025-08-25 00:18:52 -0700

    traking hidden and expanded fuctionality added

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const organicVendorRoutes = require('./routes/organicVendors');
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/csv-import-test');
 
 // Middleware
 app.use(express.json());
